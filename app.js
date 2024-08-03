df21aca2d20eef47d09495a9d7bac8d42342bf00
Mariia Posselt <maruw@ya.ru>
2024-08-03 20:42:50 +0200

    set new colors by pressing space

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const colorSections = document.querySelectorAll('[data-js="color"]');
 
 const hexCodes = "0123456789ABCDEF";
 
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    setRandomColors();
+  }
+});
+
 function generateRandomColor() {
   let color = "#";
   for (let i = 0; i < 6; i++) {
@@ -14,9 +20,10 @@ function setRandomColors() {
   colorSections.forEach((colorSection) => {
     const color = generateRandomColor();
     const heading = colorSection.querySelector("h2");
+    const lock = colorSection.querySelector("button");
     heading.textContent = color;
-    heading.style.color = calculateLuminance(color) >= 0.18 ? "black" : "white";
-    console.log(calculateLuminance(color));
+    heading.style.color = calculateLuminance(color) >= 0.5 ? "black" : "white";
+    lock.style.color = calculateLuminance(color) >= 0.5 ? "black" : "white";
     colorSection.style.background = color;
   });
 }
@@ -27,6 +34,5 @@ function calculateLuminance(hexColor) {
   let r = parseInt(hexColor.substring(1, 3), 16) / 255;
   let g = parseInt(hexColor.substring(3, 5), 16) / 255;
   let b = parseInt(hexColor.substring(5, 7), 16) / 255;
-
   return 0.2126 * r + 0.7152 * g + 0.0722 * b;
 }
