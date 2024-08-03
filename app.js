611c572e31b2305fabd388e484eafa870774df95
Mariia Posselt <maruw@ya.ru>
2024-08-03 20:25:34 +0200

    make text color black/white depending on background luminance

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ function generateRandomColor() {
   for (let i = 0; i < 6; i++) {
     color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
   }
-  console.log(Math.floor(Math.random() * hexCodes.length));
   return color;
 }
 
 function setRandomColors() {
   colorSections.forEach((colorSection) => {
-    colorSection.style.background = generateRandomColor();
+    const color = generateRandomColor();
+    const heading = colorSection.querySelector("h2");
+    heading.textContent = color;
+    heading.style.color = calculateLuminance(color) >= 0.18 ? "black" : "white";
+    console.log(calculateLuminance(color));
+    colorSection.style.background = color;
   });
 }
 
 setRandomColors();
+
+function calculateLuminance(hexColor) {
+  let r = parseInt(hexColor.substring(1, 3), 16) / 255;
+  let g = parseInt(hexColor.substring(3, 5), 16) / 255;
+  let b = parseInt(hexColor.substring(5, 7), 16) / 255;
+
+  return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+}
